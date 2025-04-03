1030427641915e42778f858d14fa5cc39fbd490f
diego <diegoboliveira@gmail.com>
2025-04-03 14:21:55 -0300

    api initiL commit

---
+++ keycloak-admin-api/src/test/java/com/api/keycloakadmin/keycloak_admin_api/KeycloakAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.keycloakadmin.keycloak_admin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAdminApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
