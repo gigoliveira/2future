7b65baa98d96ec24c4d2112640a641b01064e60c
diego <diegoboliveira@gmail.com>
2025-04-03 11:59:05 -0300

    api project initial commit

---
+++ keycloak-admin-api/keycloakadmin/src/main/java/com/keycloakadmin/keycloakadmin/KeycloakadminApplication.java
@@ -0,0 +1,13 @@
+package com.keycloakadmin.keycloakadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakadminApplication.class, args);
+	}
+
+}
---
+++ keycloak-admin-api/keycloakadmin/src/test/java/com/keycloakadmin/keycloakadmin/KeycloakadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keycloakadmin.keycloakadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
